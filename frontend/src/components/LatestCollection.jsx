bdeccf20a9171d692ea4fe04cd44a5e6d58b2318
GabrielEsquiavoni <gabrielazevedo.2004@gmail.com>
2026-02-03 19:28:51 -0300

    refactor: simplify LatestCollection

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,36 +1,34 @@
-import React, {useContext, useEffect, useState} from 'react'
-import { ShopContext } from '../context/shopContext'
+import React, { useContext, useMemo } from 'react'
+import { ShopContext } from '../context/ShopContext'
 import Title from './Title'
 import ProductItem from './ProductItem'
 
 const LatestCollection = () => {
     
-    const {products} = useContext(ShopContext);
-    const [latestProducts, setLatestProducts] = useState([])
-  
-    useEffect(() => {
-        setLatestProducts(products.slice(0,10))
-    }, [])
+    const { products } = useContext(ShopContext);
+
+    const latestProducts = useMemo(() => {
+        return products.slice(0, 10);
+    }, [products]);
 
     return (
-    <div className='my-10'>
-        <div className="text-center py-8 text-3xl">
-            <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
-            <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
-            PlaceHolder Gigantesco
-            </p>
-        </div>
+        <div className='my-10'>
+            <div className="text-center py-8 text-3xl">
+                <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
+                <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia eaque dolorum esse earum, commodi quae temporibus molestiae cumque quam? Laborum doloribus facilis qui rerum, at deleniti dicta?
+                </p>
+            </div>
 
-        {/* Rendering Products*/}
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-            {
-                latestProducts.map((item,index)=>(
-                    <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
-                ))
-            }
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+                {
+                    latestProducts.map((item, index) => (
+                        <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+                    ))
+                }
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default LatestCollection
