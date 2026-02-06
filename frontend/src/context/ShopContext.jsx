da7c7687d7e68fb37b3b7fd513ba1eb524b7d699
GabrielEsquiavoni <gabrielazevedo.2004@gmail.com>
2026-02-06 00:10:36 -0300

    feat: enhance Cart component

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const ShopContext = createContext();
@@ -12,6 +13,7 @@ const ShopContextProvider = (props) => {
     const [search, setSearch] = useState("");
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState([]);
+    const navigate = useNavigate();
 
     const addToCart = async (itemId, size) => {
 
@@ -60,7 +62,7 @@ const ShopContextProvider = (props) => {
         setCartItems(cartData);
     }
 
-    const getCartAmount = async  () => {
+    const getCartAmount = () => {
         let totalAmount = 0;
         for(const items in cartItems){
             let itemInfo = products.find((product)=> product._id === items);
@@ -83,7 +85,7 @@ const ShopContextProvider = (props) => {
         search, setSearch, showSearch, setShowSearch,
         cartItems, setCartItems, addToCart,
         getCartCount, updateQuantity,
-        getCartAmount
+        getCartAmount, navigate
     }
 
     return (
