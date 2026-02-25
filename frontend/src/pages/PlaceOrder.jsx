88bfa92f91584acf65312bdd701a6b03400357cb
GabrielEsquiavoni <gabrielazevedo.2004@gmail.com>
2026-02-24 22:06:20 -0300

    feat: add payment verification functionality for Stripe integration

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -37,6 +37,23 @@ const PlaceOrder = () => {
     setFormData((data) => ({ ...data, [name]: value }));
   };
 
+  const initPay = (order) => {
+    const options = {
+      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+      amount: order.amount,
+      currency: order.currency,
+      name: "Order Payment",
+      description: "Order Payment",
+      order_id: order.id,
+      receipt: order.receipt,
+      handler: async (response) => {
+        console.log(response);
+      },
+    };
+    const rzp = new window.Razorpay(options);
+    rzp.open();
+  };
+
   const onSubmitHandler = async (event) => {
     event.preventDefault();
     try {
@@ -61,13 +78,13 @@ const PlaceOrder = () => {
         amount: getCartAmount() + delivery_fee,
       };
       switch (method) {
-        // API Calls for COD
-        case "cod":
+        case "cod": {
           const response = await axios.post(
             backendUrl + "/api/order/place",
             orderData,
             { headers: { token } },
           );
+
           if (response.data.success) {
             setCartItems({});
             navigate("/orders");
@@ -75,6 +92,40 @@ const PlaceOrder = () => {
             toast.error(response.data.message);
           }
           break;
+        }
+
+        case "stripe": {
+          const responseStripe = await axios.post(
+            backendUrl + "/api/orders/stripe",
+            orderData,
+            { headers: { token } },
+          );
+
+          if (responseStripe.data.success) {
+            const { session_url } = responseStripe.data;
+            window.location.replace(session_url);
+          } else {
+            toast.error(responseStripe.data.message);
+          }
+          break;
+        }
+
+        case "razorpay": {
+          const responseRazorpay = await axios.post(
+            backendUrl + "/api/orders/razorpay",
+            orderData,
+            { headers: { token } },
+          );
+
+          if (responseRazorpay.data.success) {
+            initPay(responseRazorpay.data.order);
+          } else {
+            toast.error(responseRazorpay.data.message);
+          }
+
+          break;
+        }
+
         default:
           break;
       }
